dc2238a6ca4eeb75794b382a3a8f9802d126f237
Stefan Insam <ramsesoriginal@gmail.com>
2014-10-04 15:47:17 +0200

    Responsify!

--- js/masonryStarter.js
+++ js/masonryStarter.js
@@ -1,6 +1,22 @@
-var $container = $('main');
-  var em = parseFloat( $container.css('font-size') );
-  $container.masonry({
-    columnWidth: 5 * em,
-    itemSelector: 'article'
-  });
+
+var mq = window.matchMedia("(min-width: 1000px)");
+mq.addListener(WidthChange);
+WidthChange(mq);
+
+function WidthChange(mq) {
+	var $container = $('main');
+	var em = parseFloat( $container.css('font-size') );
+	if (mq.matches) {
+		// window width is at least 1000px
+		  $container.masonry({
+		    columnWidth: 5 * em,
+		    itemSelector: 'article'
+		  });
+	}
+	else {
+		// window width is less than 1000px
+		$container.masonry( 'destroy' );
+	}
+
+}
+
