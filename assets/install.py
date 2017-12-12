65abb8f408f49e8fbb5bea46392e4dca91c625b1
Stefan Insam <ramsesoriginal@gmail.com>
2017-12-12 02:39:25 +0100

    Add files via upload

---
+++ assets/install.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import urllib.request
+import winreg
+import subprocess
+
+
+files = ["comaJudgeHandler.py",
+         "commandLineHandler.py",
+         "remoteHandler.py",
+         "remoteLoop.py",
+         "systemHandler.py",
+         "testHandler.py"]
+starter = "comajudgetool.bat"
+localfolder = os.environ['USERPROFILE'] + "\\AppData\\Roaming\\sinsam\\comajudgetools\\"
+libfolder = localfolder + "lib\\"
+base_url = "http://ramsesoriginal.info/assets/"
+
+if not os.path.exists(localfolder):
+    print("Creating Application Folder")
+    os.makedirs(localfolder)
+if not os.path.exists(libfolder):
+    os.makedirs(libfolder)
+for file in files:
+    print("Downloading " + file)
+    urllib.request.urlretrieve(base_url + file, libfolder + file)
+print("Downloading " + starter)
+urllib.request.urlretrieve(base_url + starter, localfolder + starter)
+
+key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r'Environment', 0, winreg.KEY_READ)
+val = winreg.QueryValueEx(key, "PATH")[0]
+
+res = command = 'setx PATH "' + val + ';' + localfolder + ''
+if localfolder not in val:
+    print("Adding to %PATH%")
+    try:
+        subprocess.run(command, shell=True, timeout=30)
+        print("Added")
+    except Exception:
+        pass
+print("Installation Complete, enjoy the comajudgetool")
